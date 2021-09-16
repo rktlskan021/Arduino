d2d9e25bc35eddf1cb929157b86d3676fe85977f
rktlskan021 <rktlskan021@gmail.com>
2021-09-16 19:16:54 +0900

    Create 05_practice_2.ino
    창업연계공학설계입문 3주차 과제

---
+++ 05_practice_2/05_practice_2.ino
@@ -0,0 +1,20 @@
+#define PIN7 7
+
+void setup() {
+  pinMode(PIN7, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(PIN7, HIGH);
+  delay(1000);
+  for(int i = 0;i<5;i++){
+      digitalWrite(PIN7, LOW);
+    delay(100);
+    digitalWrite(PIN7, HIGH);
+    delay(100);
+  }
+  digitalWrite(PIN7, LOW);
+  while(1){
+    
+  }
+}
